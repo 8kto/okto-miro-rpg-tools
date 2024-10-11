97d3772d43a085741786578c596547ee6bd43446
8kto <ifhtagn@gmail.com>
2024-10-11 16:38:04 +0200

    Prepare token exports

---
+++ src/data/tokenExports.ts
@@ -0,0 +1,20 @@
+// Auto-generated token exports
+
+import CentipedeToken from '../images/tokens/rounded/centipede.png'
+import GcubeToken from '../images/tokens/rounded/gcube.png'
+import GhoulToken from '../images/tokens/rounded/ghoul.png'
+import GoblinToken from '../images/tokens/rounded/goblin.png'
+import RatToken from '../images/tokens/rounded/rat.png'
+import SkeletonToken from '../images/tokens/rounded/skeleton.png'
+import ZombieToken from '../images/tokens/rounded/zombie.png'
+
+
+export default {
+  CentipedeToken,
+  GcubeToken,
+  GhoulToken,
+  GoblinToken,
+  RatToken,
+  SkeletonToken,
+  ZombieToken,
+}
