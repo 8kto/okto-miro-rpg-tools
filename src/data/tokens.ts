201c318837750af4fe11e829ac9ffa816da624c4
8kto <ifhtagn@gmail.com>
2024-10-11 15:12:04 +0200

    Extract tokens data

---
+++ src/data/tokens.ts
@@ -0,0 +1,12 @@
+export type Token = {
+  src: string
+  title: string
+}
+
+export const tokens: Token[] = [
+  {src: "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-projects.svg", title: "Image 1"},
+  {src: "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-account.svg", title: "Image 2"},
+  {src: "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-product.svg", title: "Image 3"},
+  {src: "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-ux-research.svg", title: "Image 4"},
+  {src: "https://static-website.miro.com/static/images/page/mr-features-1/tour-m-learn.svg", title: "Image 5"},
+];
