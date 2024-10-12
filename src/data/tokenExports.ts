b7ff4467e28034628333ef1b39d03a8b82df44ef
8kto <ifhtagn@gmail.com>
2024-10-12 13:54:42 +0200

    Extend tokens

--- src/data/tokenExports.ts
+++ src/data/tokenExports.ts
@@ -1,18 +1,34 @@
 // Auto-generated token exports
 
-import CentipedeToken from "../images/tokens/rounded/centipede.png"
-import GelatinousCubeToken from "../images/tokens/rounded/gelatinousCube.png"
-import GhoulToken from "../images/tokens/rounded/ghoul.png"
-import GoblinToken from "../images/tokens/rounded/goblin.png"
-import RatToken from "../images/tokens/rounded/rat.png"
-import SkeletonToken from "../images/tokens/rounded/skeleton.png"
-import ZombieToken from "../images/tokens/rounded/zombie.png"
+import CatPersonToken from '../images/tokens/rounded/catPerson.png'
+import CentipedeToken from '../images/tokens/rounded/centipede.png'
+import ElfArcherToken from '../images/tokens/rounded/elfArcher.png'
+import GelatinousCubeToken from '../images/tokens/rounded/gelatinousCube.png'
+import GhoulToken from '../images/tokens/rounded/ghoul.png'
+import GoblinToken from '../images/tokens/rounded/goblin.png'
+import HalfOrc2Token from '../images/tokens/rounded/halfOrc2.png'
+import HalfOrc3Token from '../images/tokens/rounded/halfOrc3.png'
+import HalfOrcToken from '../images/tokens/rounded/halfOrc.png'
+import HumanToken from '../images/tokens/rounded/human.png'
+import Knight1Token from '../images/tokens/rounded/knight1.png'
+import PaladinToken from '../images/tokens/rounded/paladin.png'
+import RatToken from '../images/tokens/rounded/rat.png'
+import SkeletonToken from '../images/tokens/rounded/skeleton.png'
+import ZombieToken from '../images/tokens/rounded/zombie.png'
 
 const tokens = {
+  CatPersonToken,
   CentipedeToken,
+  ElfArcherToken,
   GelatinousCubeToken,
   GhoulToken,
   GoblinToken,
+  HalfOrc2Token,
+  HalfOrc3Token,
+  HalfOrcToken,
+  HumanToken,
+  Knight1Token,
+  PaladinToken,
   RatToken,
   SkeletonToken,
   ZombieToken,
