3103ca792fa8cbda1c45c7d2f3df82b67b6d83c4
8kto <ifhtagn@gmail.com>
2024-10-12 13:37:24 +0200

    Update token exports

--- src/data/tokenExports.ts
+++ src/data/tokenExports.ts
@@ -1,22 +1,19 @@
 // Auto-generated token exports
 
 import CentipedeToken from "../images/tokens/rounded/centipede.png"
-import GcubeToken from "../images/tokens/rounded/gcube.png"
+import GelatinousCubeToken from "../images/tokens/rounded/gelatinousCube.png"
 import GhoulToken from "../images/tokens/rounded/ghoul.png"
 import GoblinToken from "../images/tokens/rounded/goblin.png"
 import RatToken from "../images/tokens/rounded/rat.png"
 import SkeletonToken from "../images/tokens/rounded/skeleton.png"
 import ZombieToken from "../images/tokens/rounded/zombie.png"
 
-const tokens = {
+export default {
   CentipedeToken,
-  GcubeToken,
+  GelatinousCubeToken,
   GhoulToken,
   GoblinToken,
   RatToken,
   SkeletonToken,
   ZombieToken,
 }
-
-export type TokenDict = typeof tokens
-export default tokens
