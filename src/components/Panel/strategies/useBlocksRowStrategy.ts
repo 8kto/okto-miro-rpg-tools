53cf1ce0c21e97e7d5e6a881a0aa92478352007e
8kto <ifhtagn@gmail.com>
2024-10-12 13:06:00 +0200

    Refactor strategies

--- src/components/Panel/strategies/useBlocksRowStrategy.ts
+++ src/components/Panel/strategies/useBlocksRowStrategy.ts
@@ -1,5 +1,6 @@
-import { GroupableItem } from '@mirohq/websdk-types/stable/features/widgets/group'
-import { getSpacing } from '../utils.getSpacing'
+import { GroupableItem } from "@mirohq/websdk-types/stable/features/widgets/group"
+import { getSpacing } from "../utils.getSpacing"
+import { StrategyProps } from "./index"
 
 const { board } = miro
 
@@ -8,55 +9,46 @@ export const useBlocksRowStrategy = async ({
   y,
   width,
   n = 8,
-  tokenSize
-}: {
-  x: number;
-  y: number;
-  n: number;
-  width: number;
-  tokenSize: number;
-}): Promise<GroupableItem[]> => {
-  const MAGIC_BOTTOM_OFFSET = y + getSpacing(tokenSize);
-  const MAGIC_LEFT_OFFSET = x - width / 2.5;
-  const MAGIC_OFFSET_STEP = 15;
+  tokenSize,
+}: StrategyProps): Promise<GroupableItem[]> => {
+  const MAGIC_BOTTOM_OFFSET = y + getSpacing(tokenSize)
+  const MAGIC_LEFT_OFFSET = x - width / 2.5
+  const MAGIC_OFFSET_STEP = 15
 
-  // Create 'x' texts in parallel
   const hpTextPromises = Array.from({ length: n }, (_, i) =>
     board.createText({
-      content: 'x',
+      content: "x",
       x: MAGIC_LEFT_OFFSET + i * MAGIC_OFFSET_STEP,
       y: MAGIC_BOTTOM_OFFSET,
       width: 1,
       style: {
-        textAlign: 'center',
+        textAlign: "center",
         fontSize: 14,
-        fillColor: '#000',
-        color: '#fff',
+        fillColor: "#000",
+        color: "#fff",
       },
-    })
-  );
+    }),
+  )
 
-  // Create ' ' texts in parallel
   const spaceTextPromises = Array.from({ length: n }, (_, i) =>
     board.createText({
-      content: ' ',
+      content: " ",
       x: MAGIC_LEFT_OFFSET + i * MAGIC_OFFSET_STEP,
       y: MAGIC_BOTTOM_OFFSET,
       width: 1,
       style: {
-        textAlign: 'left',
+        textAlign: "left",
         fontSize: 14,
-        fillColor: '#fff',
-        color: '#000',
+        fillColor: "#fff",
+        color: "#000",
       },
-    })
-  );
+    }),
+  )
 
-  // Execute all promises in parallel and wait for completion
-  const [hpTexts, spaceTexts] = await Promise.all([
+  const [hpTextBlocks, spaceTextBlocks] = await Promise.all([
     Promise.all(hpTextPromises),
     Promise.all(spaceTextPromises),
-  ]);
+  ])
 
-  return hpTexts.concat(spaceTexts);
-};
+  return hpTextBlocks.concat(spaceTextBlocks)
+}
--- src/components/Panel/utils.ts
+++ src/components/Panel/utils.ts
@@ -1,43 +1,54 @@
-import { Image } from '@mirohq/websdk-types/stable/features/widgets/image'
-
-import { useBlocksRowStrategy } from './strategies'
-import { DEFAULT_TITLE_FONT_SIZE, DEFAULT_TOKEN_SIZE } from './consts'
-import { getSpacing } from './utils.getSpacing'
+import { Image } from "@mirohq/websdk-types/stable/features/widgets/image"
+import { DEFAULT_TITLE_FONT_SIZE, DEFAULT_TOKEN_SIZE } from "./consts"
+import { getSpacing } from "./utils.getSpacing"
+import { StrategyDef } from "./strategies"
 
 const { board } = miro
 
 const getTitleFontSize = (input: number): number => {
-  const ratio = DEFAULT_TITLE_FONT_SIZE / DEFAULT_TOKEN_SIZE;
-  return Math.round(input * ratio);
+  const ratio = DEFAULT_TITLE_FONT_SIZE / DEFAULT_TOKEN_SIZE
+  return Math.round(input * ratio)
 }
 
-export const getTokenTitle = async ({ x, y, title, tokenSize}: { x: number; y: number; title: string; tokenSize: number }) => {
+export const getTokenTitle = async ({
+  x,
+  y,
+  title,
+  tokenSize,
+}: {
+  x: number
+  y: number
+  title: string
+  tokenSize: number
+}) => {
   return await board.createText({
     content: title,
     x,
     y,
     style: {
-      textAlign: 'center',
+      textAlign: "center",
       fontSize: getTitleFontSize(tokenSize),
-      fillColor: '#000000',
-      color: '#fff',
+      fillColor: "#000000",
+      color: "#fff",
     },
   })
 }
 
-export const convertImageToToken = async (
-  options?: {
-    image?: Image
-    tokenSize: number
+export const convertImageToToken = async (options?: {
+  image?: Image
+  tokenSize: number
+  strategy: StrategyDef
+}) => {
+  const { image, tokenSize = DEFAULT_TOKEN_SIZE, strategy } = options || {}
+
+  if (!strategy) {
+    throw new Error("convertImageToToken: No strategy found")
   }
-) => {
-  const {
-    image,
-    tokenSize= DEFAULT_TOKEN_SIZE
-  } = options || {}
 
   const selectedWidgets = await board.getSelection()
-  const selectedImage = image || selectedWidgets.find(widget => widget.type === 'image') as Image
+  const selectedImage =
+    image ||
+    (selectedWidgets.find((widget) => widget.type === "image") as Image)
 
   if (!selectedImage) {
     return
@@ -48,11 +59,11 @@ export const convertImageToToken = async (
   const titleText = await getTokenTitle({
     x,
     y: y - getSpacing(tokenSize),
-    title: alt || 'NA',
-    tokenSize
+    title: alt || "NA",
+    tokenSize,
   })
 
-  const buffer = await useBlocksRowStrategy({ x, y, width, n: 8, tokenSize })
+  const buffer = await strategy.run({ x, y, width, n: 8, tokenSize })
 
   await board.group({ items: [selectedImage, titleText, ...buffer] })
   await titleText.sync()
