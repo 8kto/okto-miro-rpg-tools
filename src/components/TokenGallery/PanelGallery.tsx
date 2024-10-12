411359877b4f562a76f28501b04fc8c2b12c1e08
8kto <ifhtagn@gmail.com>
2024-10-12 13:06:30 +0200

    Format files

--- src/components/TokenGallery/PanelGallery.tsx
+++ src/components/TokenGallery/PanelGallery.tsx
@@ -1,32 +1,32 @@
-import { TokenDict } from '../../data/tokenExports'
+import { TokenDict } from "../../data/tokenExports"
 
 type PanelGallery = {
   tokens: TokenDict
 }
-const PanelGallery = ({
-  tokens
-}: PanelGallery) => (
-  <>
-    <h3 className="section-header">Tokens gallery</h3>
-    <div className="grid-container">
-      {Object.entries(tokens).map(([tokenTitle, token]) => {
-        const title = tokenTitle.replace('Token', '')
+const PanelGallery = ({ tokens }: PanelGallery) => {
+  return (
+    <>
+      <h3 className="h2 section-header">Token gallery</h3>
+      <div className="grid-container">
+        {Object.entries(tokens).map(([tokenTitle, token]) => {
+          const title = tokenTitle.replace("Token", "")
 
-        return (
-          <div key={title} className="grid-item">
-            <img
-              src={token}
-              draggable={false}
-              className="miro-draggable draggable-item draggable-item--image"
-              alt={title}
-              title={title}
-            />
-            <span className="token-title">{title}</span>
-          </div>
-        )
-      })}
-    </div>
-  </>
-)
+          return (
+            <div key={title} className="grid-item">
+              <img
+                src={token}
+                draggable={false}
+                className="miro-draggable draggable-item draggable-item--image"
+                alt={title}
+                title={title}
+              />
+              <span className="token-title">{title}</span>
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
 
 export default PanelGallery
