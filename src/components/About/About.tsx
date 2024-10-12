b65cbe0def13800b0c911ea32289e0e13c65da07
8kto <ifhtagn@gmail.com>
2024-10-12 16:37:28 +0200

    Display About block

---
+++ src/components/About/About.tsx
@@ -0,0 +1,48 @@
+const About = () => (
+
+  <>
+    <p>
+      <a property="dct:title" target="_blank" rel="cc:attributionURL" href="https://github.com/8kto/okto-miro-rpg-tools">
+        Okto Miro RPG Tools
+      </a>{" "}
+      by{" "}
+      <a target="_blank" rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/8kto">
+        undefined / 8kto
+      </a> (<a target="_blank" rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://ivlev.blog">
+      webpage
+    </a>)
+      {" "}
+      is licensed under{" "}
+      <a
+        href="https://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1"
+        target="_blank"
+        rel="license noopener noreferrer"
+        style="display:inline-block;"
+      >
+        CC BY-NC-SA 4.0
+        <img
+          style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+          src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
+          alt=""
+        />
+        <img
+          style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+          src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
+          alt=""
+        />
+        <img
+          style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+          src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"
+          alt=""
+        />
+        <img
+          style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+          src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"
+          alt=""
+        />
+      </a>
+    </p>
+  </>
+)
+
+export default About
