db3eb2fb36a8abc996bcf1f9a6dd6e0902d6440e
8kto <ifhtagn@gmail.com>
2024-10-12 14:52:32 +0200

    Add filter input

--- src/components/TokenGallery/TokenGallery.tsx
+++ src/components/TokenGallery/TokenGallery.tsx
@@ -1,33 +1,38 @@
 import { TokenDict } from "../../data/tokenExports"
 import { formatTokenTitle } from "../Panel/utils"
+import { useState } from "preact/compat"
+import SearchInput from "../SearchInput/SearchInput"
 
 type TokenGalleryProps = {
   tokens: TokenDict
 }
 const TokenGallery = ({ tokens }: TokenGalleryProps) => {
+  const [searchInputValue, setSearchInputValue] = useState("")
+
   return (
     <>
       <h3 className="h2 section-header">Token gallery</h3>
-      <p className="p">
-        Tokens used: Devin Night (immortalnights.com), VTT Token Maker (thefatefulforce.com)
-      </p>
+      <SearchInput handleInputChange={(value) => setSearchInputValue(value)} />
+      <p className="p">Tokens used: Devin Night (immortalnights.com), VTT Token Maker (thefatefulforce.com)</p>
       <div className="grid-container">
-        {Object.entries(tokens).map(([tokenTitle, token]) => {
-          const title = formatTokenTitle(tokenTitle)
+        {Object.entries(tokens)
+          .filter(([tokenTitle]) => tokenTitle.toLowerCase().includes(searchInputValue.toLowerCase()))
+          .map(([tokenTitle, token]) => {
+            const title = formatTokenTitle(tokenTitle)
 
-          return (
-            <div key={title} className="grid-item">
-              <img
-                src={token}
-                draggable={false}
-                className="miro-draggable draggable-item draggable-item--image"
-                alt={title}
-                title={title}
-              />
-              <span className="token-title">{title}</span>
-            </div>
-          )
-        })}
+            return (
+              <div key={title} className="grid-item">
+                <img
+                  src={token}
+                  draggable={false}
+                  className="miro-draggable draggable-item draggable-item--image"
+                  alt={title}
+                  title={title}
+                />
+                <span className="token-title">{title}</span>
+              </div>
+            )
+          })}
       </div>
     </>
   )
