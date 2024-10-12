de7a71daa34dacf92e142e1d1108496fab2955ff
8kto <ifhtagn@gmail.com>
2024-10-12 16:08:22 +0200

    Update token exports

--- src/data/tokenExports.ts
+++ src/data/tokenExports.ts
@@ -1,185 +1,95 @@
-// Auto-generated token exports
-
-import TokenCentipede from "../images/tokens/rounded/centipede.png"
-import TokenZombie from "../images/tokens/rounded/zombie.png"
-import TokenSkeleton from "../images/tokens/rounded/skeleton.png"
-import TokenLizardmanAxeShield from "../images/tokens/rounded/DevinNight/lizardmanAxeShield.png"
-import TokenGreenSlime from "../images/tokens/rounded/DevinNight/greenSlime.png"
-import TokenFighter36 from "../images/tokens/rounded/DevinNight/fighter36.png"
-import TokenRatGiant05 from "../images/tokens/rounded/DevinNight/ratGiant05.png"
-import TokenSkeletonArcherRed from "../images/tokens/rounded/DevinNight/skeletonArcherRed.png"
-import TokenFighterBlue07 from "../images/tokens/rounded/DevinNight/fighterBlue07.png"
-import TokenOgreFighter from "../images/tokens/rounded/DevinNight/ogreFighter.png"
-import TokenMagicuser44 from "../images/tokens/rounded/DevinNight/magicuser44.png"
-import TokenWizardEvil11 from "../images/tokens/rounded/DevinNight/wizardEvil11.png"
-import TokenFrogGiantRed from "../images/tokens/rounded/DevinNight/frogGiantRed.png"
-import TokenGoblinDaggers from "../images/tokens/rounded/DevinNight/goblinDaggers.png"
-import TokenArcher40 from "../images/tokens/rounded/DevinNight/archer40.png"
-import TokenWorg22 from "../images/tokens/rounded/DevinNight/worg22.png"
-import TokenDragonRed08 from "../images/tokens/rounded/DevinNight/dragonRed08.png"
-import TokenSkeletonMace from "../images/tokens/rounded/DevinNight/skeletonMace.png"
-import TokenSkeletonAxeShield from "../images/tokens/rounded/DevinNight/skeletonAxeShield.png"
-import TokenWightBone from "../images/tokens/rounded/DevinNight/wightBone.png"
-import TokenTurtleGiant from "../images/tokens/rounded/DevinNight/turtleGiant.png"
-import TokenSpiderGiant from "../images/tokens/rounded/DevinNight/spiderGiant.png"
-import TokenElvenBlueFighter09 from "../images/tokens/rounded/DevinNight/elvenBlueFighter09.png"
-import TokenLizardmanShaman from "../images/tokens/rounded/DevinNight/lizardmanShaman.png"
-import TokenThiefDaggers21 from "../images/tokens/rounded/DevinNight/thiefDaggers21.png"
-import TokenElvenGreenArcher02 from "../images/tokens/rounded/DevinNight/elvenGreenArcher02.png"
-import TokenLizardGiant from "../images/tokens/rounded/DevinNight/lizardGiant.png"
-import TokenMagicuser43 from "../images/tokens/rounded/DevinNight/magicuser43.png"
-import TokenFrogGiantGreen from "../images/tokens/rounded/DevinNight/frogGiantGreen.png"
-import TokenFighterFemaleDagger34 from "../images/tokens/rounded/DevinNight/fighterFemaleDagger34.png"
-import TokenGoblinAxeman from "../images/tokens/rounded/DevinNight/goblinAxeman.png"
-import TokenGreyOoze from "../images/tokens/rounded/DevinNight/greyOoze.png"
-import TokenKnight49 from "../images/tokens/rounded/DevinNight/knight49.png"
-import TokenZombieMaleGrren from "../images/tokens/rounded/DevinNight/zombieMaleGrren.png"
-import TokenHalfling05 from "../images/tokens/rounded/DevinNight/halfling05.png"
-import TokenKoboldShaman1 from "../images/tokens/rounded/DevinNight/koboldShaman1.png"
-import TokenFighterBlueSpear30 from "../images/tokens/rounded/DevinNight/fighterBlueSpear30.png"
-import TokenOgreBerserker from "../images/tokens/rounded/DevinNight/ogreBerserker.png"
-import TokenFighterBlueSword26 from "../images/tokens/rounded/DevinNight/fighterBlueSword26.png"
-import TokenBeetleGiant01 from "../images/tokens/rounded/DevinNight/beetleGiant01.png"
-import TokenMagicuser19 from "../images/tokens/rounded/DevinNight/magicuser19.png"
-import TokenOrcFighter from "../images/tokens/rounded/DevinNight/orcFighter.png"
-import TokenKoboldFighter from "../images/tokens/rounded/DevinNight/koboldFighter.png"
-import TokenFemaleGhostBlue2 from "../images/tokens/rounded/DevinNight/femaleGhostBlue2.png"
-import TokenGhostWhiteEvil from "../images/tokens/rounded/DevinNight/ghostWhiteEvil.png"
-import TokenCaveman01 from "../images/tokens/rounded/DevinNight/caveman01.png"
-import TokenGoblinShaman from "../images/tokens/rounded/DevinNight/goblinShaman.png"
-import TokenFighterSwordBuckler31 from "../images/tokens/rounded/DevinNight/fighterSwordBuckler31.png"
-import TokenPurpleWorm from "../images/tokens/rounded/DevinNight/purpleWorm.png"
-import TokenCockatrice from "../images/tokens/rounded/DevinNight/cockatrice.png"
-import TokenBlueSlime from "../images/tokens/rounded/DevinNight/blueSlime.png"
-import TokenOrcShaman from "../images/tokens/rounded/DevinNight/orcShaman.png"
-import TokenGiantWorm18 from "../images/tokens/rounded/DevinNight/giantWorm18.png"
-import TokenKoboldArcher from "../images/tokens/rounded/DevinNight/koboldArcher.png"
-import TokenGoblinArcher from "../images/tokens/rounded/DevinNight/goblinArcher.png"
-import TokenGargoyle from "../images/tokens/rounded/DevinNight/gargoyle.png"
-import TokenBugbearMace04 from "../images/tokens/rounded/DevinNight/bugbearMace04.png"
-import TokenLichSkullEvil from "../images/tokens/rounded/DevinNight/lichSkullEvil.png"
-import TokenClericRed47 from "../images/tokens/rounded/DevinNight/clericRed47.png"
-import TokenFighterFemale35 from "../images/tokens/rounded/DevinNight/fighterFemale35.png"
-import TokenFighterSilver10 from "../images/tokens/rounded/DevinNight/fighterSilver10.png"
-import TokenMagicuserGreen08 from "../images/tokens/rounded/DevinNight/magicuserGreen08.png"
-import TokenDwarvenFighterHammer03 from "../images/tokens/rounded/DevinNight/dwarvenFighterHammer03.png"
-import TokenMagicuser37 from "../images/tokens/rounded/DevinNight/magicuser37.png"
-import TokenDemonPit15 from "../images/tokens/rounded/DevinNight/demonPit15.png"
-import TokenRustMonster from "../images/tokens/rounded/DevinNight/rustMonster.png"
-import TokenTrollFighter from "../images/tokens/rounded/DevinNight/trollFighter.png"
-import TokenMagicuser38 from "../images/tokens/rounded/DevinNight/magicuser38.png"
-import TokenCrocodile from "../images/tokens/rounded/DevinNight/crocodile.png"
-import TokenOwlbear2 from "../images/tokens/rounded/DevinNight/owlbear2.png"
-import TokenFighterMace24 from "../images/tokens/rounded/DevinNight/fighterMace24.png"
-import TokenLizardmanSpear from "../images/tokens/rounded/DevinNight/lizardmanSpear.png"
-import TokenFighter39 from "../images/tokens/rounded/DevinNight/fighter39.png"
-import TokenFighterSword25 from "../images/tokens/rounded/DevinNight/fighterSword25.png"
-import TokenGelatinousCube from "../images/tokens/rounded/DevinNight/gelatinousCube.png"
-import TokenGoblinMace from "../images/tokens/rounded/DevinNight/goblinMace.png"
-import TokenZombieRotting12 from "../images/tokens/rounded/DevinNight/zombieRotting12.png"
-import TokenRat from "../images/tokens/rounded/rat.png"
-import TokenGhoul from "../images/tokens/rounded/ghoul.png"
-import TokenGelatinousCubeCustom from "../images/tokens/rounded/gelatinousCubeCustom.png"
-import TokenGoblin from "../images/tokens/rounded/goblin.png"
-import TokenCatPerson from "../images/tokens/rounded/FatefulForce/catPerson.png"
-import TokenHuman from "../images/tokens/rounded/FatefulForce/human.png"
-import TokenHalfOrc3 from "../images/tokens/rounded/FatefulForce/halfOrc3.png"
-import TokenHalfOrc from "../images/tokens/rounded/FatefulForce/halfOrc.png"
-import TokenHalfOrc2 from "../images/tokens/rounded/FatefulForce/halfOrc2.png"
-import TokenKnight1 from "../images/tokens/rounded/FatefulForce/knight1.png"
-import TokenElfArcher from "../images/tokens/rounded/FatefulForce/elfArcher.png"
-import TokenPaladin from "../images/tokens/rounded/FatefulForce/paladin.png"
+// Auto-generated token exports (Lazy Loaded)
 
 const tokens = {
-  TokenCentipede,
-  TokenZombie,
-  TokenSkeleton,
-  TokenLizardmanAxeShield,
-  TokenGreenSlime,
-  TokenFighter36,
-  TokenRatGiant05,
-  TokenSkeletonArcherRed,
-  TokenFighterBlue07,
-  TokenOgreFighter,
-  TokenMagicuser44,
-  TokenWizardEvil11,
-  TokenFrogGiantRed,
-  TokenGoblinDaggers,
-  TokenArcher40,
-  TokenWorg22,
-  TokenDragonRed08,
-  TokenSkeletonMace,
-  TokenSkeletonAxeShield,
-  TokenWightBone,
-  TokenTurtleGiant,
-  TokenSpiderGiant,
-  TokenElvenBlueFighter09,
-  TokenLizardmanShaman,
-  TokenThiefDaggers21,
-  TokenElvenGreenArcher02,
-  TokenLizardGiant,
-  TokenMagicuser43,
-  TokenFrogGiantGreen,
-  TokenFighterFemaleDagger34,
-  TokenGoblinAxeman,
-  TokenGreyOoze,
-  TokenKnight49,
-  TokenZombieMaleGrren,
-  TokenHalfling05,
-  TokenKoboldShaman1,
-  TokenFighterBlueSpear30,
-  TokenOgreBerserker,
-  TokenFighterBlueSword26,
-  TokenBeetleGiant01,
-  TokenMagicuser19,
-  TokenOrcFighter,
-  TokenKoboldFighter,
-  TokenFemaleGhostBlue2,
-  TokenGhostWhiteEvil,
-  TokenCaveman01,
-  TokenGoblinShaman,
-  TokenFighterSwordBuckler31,
-  TokenPurpleWorm,
-  TokenCockatrice,
-  TokenBlueSlime,
-  TokenOrcShaman,
-  TokenGiantWorm18,
-  TokenKoboldArcher,
-  TokenGoblinArcher,
-  TokenGargoyle,
-  TokenBugbearMace04,
-  TokenLichSkullEvil,
-  TokenClericRed47,
-  TokenFighterFemale35,
-  TokenFighterSilver10,
-  TokenMagicuserGreen08,
-  TokenDwarvenFighterHammer03,
-  TokenMagicuser37,
-  TokenDemonPit15,
-  TokenRustMonster,
-  TokenTrollFighter,
-  TokenMagicuser38,
-  TokenCrocodile,
-  TokenOwlbear2,
-  TokenFighterMace24,
-  TokenLizardmanSpear,
-  TokenFighter39,
-  TokenFighterSword25,
-  TokenGelatinousCube,
-  TokenGoblinMace,
-  TokenZombieRotting12,
-  TokenRat,
-  TokenGhoul,
-  TokenGelatinousCubeCustom,
-  TokenGoblin,
-  TokenCatPerson,
-  TokenHuman,
-  TokenHalfOrc3,
-  TokenHalfOrc,
-  TokenHalfOrc2,
-  TokenKnight1,
-  TokenElfArcher,
-  TokenPaladin,
+  TokenCentipede: () => import("../images/tokens/rounded/centipede.png"),
+  TokenZombie: () => import("../images/tokens/rounded/zombie.png"),
+  TokenSkeleton: () => import("../images/tokens/rounded/skeleton.png"),
+  TokenLizardmanAxeShield: () => import("../images/tokens/rounded/DevinNight/lizardmanAxeShield.png"),
+  TokenGreenSlime: () => import("../images/tokens/rounded/DevinNight/greenSlime.png"),
+  TokenFighter36: () => import("../images/tokens/rounded/DevinNight/fighter36.png"),
+  TokenRatGiant05: () => import("../images/tokens/rounded/DevinNight/ratGiant05.png"),
+  TokenSkeletonArcherRed: () => import("../images/tokens/rounded/DevinNight/skeletonArcherRed.png"),
+  TokenFighterBlue07: () => import("../images/tokens/rounded/DevinNight/fighterBlue07.png"),
+  TokenOgreFighter: () => import("../images/tokens/rounded/DevinNight/ogreFighter.png"),
+  TokenMagicuser44: () => import("../images/tokens/rounded/DevinNight/magicuser44.png"),
+  TokenWizardEvil11: () => import("../images/tokens/rounded/DevinNight/wizardEvil11.png"),
+  TokenFrogGiantRed: () => import("../images/tokens/rounded/DevinNight/frogGiantRed.png"),
+  TokenGoblinDaggers: () => import("../images/tokens/rounded/DevinNight/goblinDaggers.png"),
+  TokenArcher40: () => import("../images/tokens/rounded/DevinNight/archer40.png"),
+  TokenWorg22: () => import("../images/tokens/rounded/DevinNight/worg22.png"),
+  TokenDragonRed08: () => import("../images/tokens/rounded/DevinNight/dragonRed08.png"),
+  TokenSkeletonMace: () => import("../images/tokens/rounded/DevinNight/skeletonMace.png"),
+  TokenSkeletonAxeShield: () => import("../images/tokens/rounded/DevinNight/skeletonAxeShield.png"),
+  TokenWightBone: () => import("../images/tokens/rounded/DevinNight/wightBone.png"),
+  TokenTurtleGiant: () => import("../images/tokens/rounded/DevinNight/turtleGiant.png"),
+  TokenSpiderGiant: () => import("../images/tokens/rounded/DevinNight/spiderGiant.png"),
+  TokenElvenBlueFighter09: () => import("../images/tokens/rounded/DevinNight/elvenBlueFighter09.png"),
+  TokenLizardmanShaman: () => import("../images/tokens/rounded/DevinNight/lizardmanShaman.png"),
+  TokenThiefDaggers21: () => import("../images/tokens/rounded/DevinNight/thiefDaggers21.png"),
+  TokenElvenGreenArcher02: () => import("../images/tokens/rounded/DevinNight/elvenGreenArcher02.png"),
+  TokenLizardGiant: () => import("../images/tokens/rounded/DevinNight/lizardGiant.png"),
+  TokenMagicuser43: () => import("../images/tokens/rounded/DevinNight/magicuser43.png"),
+  TokenFrogGiantGreen: () => import("../images/tokens/rounded/DevinNight/frogGiantGreen.png"),
+  TokenFighterFemaleDagger34: () => import("../images/tokens/rounded/DevinNight/fighterFemaleDagger34.png"),
+  TokenGoblinAxeman: () => import("../images/tokens/rounded/DevinNight/goblinAxeman.png"),
+  TokenGreyOoze: () => import("../images/tokens/rounded/DevinNight/greyOoze.png"),
+  TokenKnight49: () => import("../images/tokens/rounded/DevinNight/knight49.png"),
+  TokenZombieMaleGrren: () => import("../images/tokens/rounded/DevinNight/zombieMaleGrren.png"),
+  TokenHalfling05: () => import("../images/tokens/rounded/DevinNight/halfling05.png"),
+  TokenKoboldShaman1: () => import("../images/tokens/rounded/DevinNight/koboldShaman1.png"),
+  TokenFighterBlueSpear30: () => import("../images/tokens/rounded/DevinNight/fighterBlueSpear30.png"),
+  TokenOgreBerserker: () => import("../images/tokens/rounded/DevinNight/ogreBerserker.png"),
+  TokenFighterBlueSword26: () => import("../images/tokens/rounded/DevinNight/fighterBlueSword26.png"),
+  TokenBeetleGiant01: () => import("../images/tokens/rounded/DevinNight/beetleGiant01.png"),
+  TokenMagicuser19: () => import("../images/tokens/rounded/DevinNight/magicuser19.png"),
+  TokenOrcFighter: () => import("../images/tokens/rounded/DevinNight/orcFighter.png"),
+  TokenKoboldFighter: () => import("../images/tokens/rounded/DevinNight/koboldFighter.png"),
+  TokenFemaleGhostBlue2: () => import("../images/tokens/rounded/DevinNight/femaleGhostBlue2.png"),
+  TokenGhostWhiteEvil: () => import("../images/tokens/rounded/DevinNight/ghostWhiteEvil.png"),
+  TokenCaveman01: () => import("../images/tokens/rounded/DevinNight/caveman01.png"),
+  TokenGoblinShaman: () => import("../images/tokens/rounded/DevinNight/goblinShaman.png"),
+  TokenFighterSwordBuckler31: () => import("../images/tokens/rounded/DevinNight/fighterSwordBuckler31.png"),
+  TokenPurpleWorm: () => import("../images/tokens/rounded/DevinNight/purpleWorm.png"),
+  TokenCockatrice: () => import("../images/tokens/rounded/DevinNight/cockatrice.png"),
+  TokenBlueSlime: () => import("../images/tokens/rounded/DevinNight/blueSlime.png"),
+  TokenOrcShaman: () => import("../images/tokens/rounded/DevinNight/orcShaman.png"),
+  TokenGiantWorm18: () => import("../images/tokens/rounded/DevinNight/giantWorm18.png"),
+  TokenKoboldArcher: () => import("../images/tokens/rounded/DevinNight/koboldArcher.png"),
+  TokenGoblinArcher: () => import("../images/tokens/rounded/DevinNight/goblinArcher.png"),
+  TokenGargoyle: () => import("../images/tokens/rounded/DevinNight/gargoyle.png"),
+  TokenBugbearMace04: () => import("../images/tokens/rounded/DevinNight/bugbearMace04.png"),
+  TokenLichSkullEvil: () => import("../images/tokens/rounded/DevinNight/lichSkullEvil.png"),
+  TokenClericRed47: () => import("../images/tokens/rounded/DevinNight/clericRed47.png"),
+  TokenFighterFemale35: () => import("../images/tokens/rounded/DevinNight/fighterFemale35.png"),
+  TokenFighterSilver10: () => import("../images/tokens/rounded/DevinNight/fighterSilver10.png"),
+  TokenMagicuserGreen08: () => import("../images/tokens/rounded/DevinNight/magicuserGreen08.png"),
+  TokenDwarvenFighterHammer03: () => import("../images/tokens/rounded/DevinNight/dwarvenFighterHammer03.png"),
+  TokenMagicuser37: () => import("../images/tokens/rounded/DevinNight/magicuser37.png"),
+  TokenDemonPit15: () => import("../images/tokens/rounded/DevinNight/demonPit15.png"),
+  TokenRustMonster: () => import("../images/tokens/rounded/DevinNight/rustMonster.png"),
+  TokenTrollFighter: () => import("../images/tokens/rounded/DevinNight/trollFighter.png"),
+  TokenMagicuser38: () => import("../images/tokens/rounded/DevinNight/magicuser38.png"),
+  TokenCrocodile: () => import("../images/tokens/rounded/DevinNight/crocodile.png"),
+  TokenOwlbear2: () => import("../images/tokens/rounded/DevinNight/owlbear2.png"),
+  TokenFighterMace24: () => import("../images/tokens/rounded/DevinNight/fighterMace24.png"),
+  TokenLizardmanSpear: () => import("../images/tokens/rounded/DevinNight/lizardmanSpear.png"),
+  TokenFighter39: () => import("../images/tokens/rounded/DevinNight/fighter39.png"),
+  TokenFighterSword25: () => import("../images/tokens/rounded/DevinNight/fighterSword25.png"),
+  TokenGelatinousCube: () => import("../images/tokens/rounded/DevinNight/gelatinousCube.png"),
+  TokenGoblinMace: () => import("../images/tokens/rounded/DevinNight/goblinMace.png"),
+  TokenZombieRotting12: () => import("../images/tokens/rounded/DevinNight/zombieRotting12.png"),
+  TokenRat: () => import("../images/tokens/rounded/rat.png"),
+  TokenGhoul: () => import("../images/tokens/rounded/ghoul.png"),
+  TokenGelatinousCubeCustom: () => import("../images/tokens/rounded/gelatinousCubeCustom.png"),
+  TokenGoblin: () => import("../images/tokens/rounded/goblin.png"),
+  TokenCatPerson: () => import("../images/tokens/rounded/FatefulForce/catPerson.png"),
+  TokenHuman: () => import("../images/tokens/rounded/FatefulForce/human.png"),
+  TokenHalfOrc3: () => import("../images/tokens/rounded/FatefulForce/halfOrc3.png"),
+  TokenHalfOrc: () => import("../images/tokens/rounded/FatefulForce/halfOrc.png"),
+  TokenHalfOrc2: () => import("../images/tokens/rounded/FatefulForce/halfOrc2.png"),
+  TokenKnight1: () => import("../images/tokens/rounded/FatefulForce/knight1.png"),
+  TokenElfArcher: () => import("../images/tokens/rounded/FatefulForce/elfArcher.png"),
+  TokenPaladin: () => import("../images/tokens/rounded/FatefulForce/paladin.png"),
 }
 
 export default tokens
