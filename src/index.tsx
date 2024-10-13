73ee097cf4c050fb59cc499d8aa6cad015f65c0b
8kto <ifhtagn@gmail.com>
2024-10-13 14:32:39 +0200

    Display dice roller

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import "/assets/style.css"
 import Panel from "./components/Panel/Panel"
+import DiceRoller from "./components/DiceRoller/DiceRoller"
+import { dices } from "./data/dices"
 
 export default function App() {
   return (
     <div id="root">
+      <DiceRoller dices={dices} />
       <Panel />
     </div>
   )
