289d5dd1a242fbf79d550376d25f421bbd69af07
8kto <ifhtagn@gmail.com>
2025-08-30 16:02:10 +0200

    Format

--- src/data/dices.ts
+++ src/data/dices.ts
@@ -15,51 +15,35 @@ const getFormattedRoll = (formula: string): [number, string] => {
 export const dices: DiceAction[] = [
   {
     title: "d4",
-    action: () => {
-      return getFormattedRoll("d4")
-    },
+    action: () => getFormattedRoll("d4"),
   },
   {
     title: "d6",
-    action: () => {
-      return getFormattedRoll("d6")
-    },
+    action: () => getFormattedRoll("d6"),
   },
   {
     title: "d8",
-    action: () => {
-      return getFormattedRoll("d8")
-    },
+    action: () => getFormattedRoll("d8"),
   },
   {
     title: "d10",
-    action: () => {
-      return getFormattedRoll("d10")
-    },
+    action: () => getFormattedRoll("d10"),
   },
   {
     title: "d12",
-    action: () => {
-      return getFormattedRoll("d12")
-    },
+    action: () => getFormattedRoll("d12"),
   },
   {
     title: "d20",
-    action: () => {
-      return getFormattedRoll("d20")
-    },
+    action: () => getFormattedRoll("d20"),
   },
   {
     title: "d100",
-    action: () => {
-      return getFormattedRoll("d100")
-    },
+    action: () => getFormattedRoll("d100"),
   },
   {
     title: "2d6",
-    action: () => {
-      return getFormattedRoll("2d6")
-    },
+    action: () => getFormattedRoll("2d6"),
     type: "secondary",
   },
   { title: "d20 + 1", action: () => getFormattedRoll("d20 + 1"), type: "secondary" },
