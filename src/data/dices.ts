f60793068376503c4839d752efd4c118716bb3d7
8kto <ifhtagn@gmail.com>
2025-08-30 15:58:51 +0200

    Change the results type

--- src/data/dices.ts
+++ src/data/dices.ts
@@ -1,29 +1,86 @@
-import { Dice, roll, rollDiceFormula } from "ttrpg-lib-dice"
+import { rollDiceFormula, rollDiceFormulaDetailed } from "ttrpg-lib-dice"
+import { formatDiceRollResult } from "../utils/format"
 
 export type DiceAction = {
   title: string
-  action: () => number | string
+  action: () => [number | null, string]
   type?: "primary" | "secondary"
 }
 
+const getFormattedRoll = (formula: string): [number, string] => {
+  const res = rollDiceFormulaDetailed(formula)
+  return [res.total, formatDiceRollResult(res)]
+}
+
 export const dices: DiceAction[] = [
-  { title: "d4", action: () => roll(Dice.d4) },
-  { title: "d6", action: () => roll(Dice.d6) },
-  { title: "d8", action: () => roll(Dice.d8) },
-  { title: "d10", action: () => roll(Dice.d10) },
-  { title: "d12", action: () => roll(Dice.d12) },
-  { title: "d20", action: () => roll(Dice.d20) },
-  { title: "d100", action: () => roll(Dice.d100) },
-  { title: "2d6", action: () => rollDiceFormula("2d6"), type: "secondary" },
-  { title: "d20 + 1", action: () => rollDiceFormula("d20 + 1"), type: "secondary" },
   {
-    title: "Stats",
+    title: "d4",
     action: () => {
-      return Array.from({ length: 6 })
-        .map(() => rollDiceFormula("3d6"))
-        .join(", ")
+      return getFormattedRoll("d4")
+    },
+  },
+  {
+    title: "d6",
+    action: () => {
+      return getFormattedRoll("d6")
+    },
+  },
+  {
+    title: "d8",
+    action: () => {
+      return getFormattedRoll("d8")
+    },
+  },
+  {
+    title: "d10",
+    action: () => {
+      return getFormattedRoll("d10")
+    },
+  },
+  {
+    title: "d12",
+    action: () => {
+      return getFormattedRoll("d12")
+    },
+  },
+  {
+    title: "d20",
+    action: () => {
+      return getFormattedRoll("d20")
+    },
+  },
+  {
+    title: "d100",
+    action: () => {
+      return getFormattedRoll("d100")
+    },
+  },
+  {
+    title: "2d6",
+    action: () => {
+      return getFormattedRoll("2d6")
     },
     type: "secondary",
   },
-  { title: "Gold", action: () => rollDiceFormula("3d6") * 10, type: "secondary" },
+  { title: "d20 + 1", action: () => getFormattedRoll("d20 + 1"), type: "secondary" },
+  {
+    title: "Stats",
+    action: () => {
+      return [
+        null,
+        Array.from({ length: 6 })
+          .map(() => rollDiceFormula("3d6"))
+          .join(", "),
+      ]
+    },
+    type: "secondary",
+  },
+  {
+    title: "Gold",
+    action: () => {
+      const res = rollDiceFormulaDetailed("3d6")
+      return [res.total * 10, `(${formatDiceRollResult(res)}) * 10`]
+    },
+    type: "secondary",
+  },
 ]
